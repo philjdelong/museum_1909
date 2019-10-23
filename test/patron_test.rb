4c8cb25cc8d8e9e085919b02f509c7d494a79d36
Phil DeLong <philjdelong@gmail.com>
2019-10-23 09:15:31 -0600

    Add add_interests and pass test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -18,4 +18,10 @@ class PatronTest < Minitest::Test
     assert_equal 20, @bob.spending_money
     assert_equal [], @bob.interests
   end
+
+  def test_it_can_add_interests
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @bob.interests
+  end
 end
