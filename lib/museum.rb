c29fd2ab97632c95a4ecd968604330d23638e444
Phil DeLong <philjdelong@gmail.com>
2019-10-23 10:42:23 -0600

    Work through recommendation test. Stuck on comparison

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,12 +1,26 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(new_exhibit)
     @exhibits << new_exhibit
   end
+
+  def add_patron(new_patron)
+    @patrons << new_patron
+  end
+
+  def recommend_exhibits(patron)
+    x = patron.interests
+    y = @exhibits.map do |exhibit|
+      exhibit.name
+    end
+    z = x & y
+  end
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -9,6 +9,11 @@ class MuseumTest < Minitest::Test
 
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
+    @bob = Patron.new("Bob", 20)
+    @sally = Patron.new("Sally", 20)
   end
 
   def test_it_exists
@@ -18,5 +23,31 @@ class MuseumTest < Minitest::Test
   def test_it_initializes_with_name_and_exhibits
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_it_can_add_patrons
+    @dmns.add_patron(@bob)
+    @dmns.add_patron(@sally)
+    assert_equal [@bob, @sally], @dmns.patrons
+  end
+
+  def test_it_can_recommend_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @dmns.add_patron(@bob)
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @dmns.add_patron(@sally)
+    @sally.add_interest("IMAX")
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@bob)
+    assert_equal [@imax], @dmns.recommend_exhibits(@sally)
   end
 end
